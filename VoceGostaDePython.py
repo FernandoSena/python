b39f25ab35cf166c831c8820bc665c06dfa40558
Fernando Sena <fernandocarvalho.sena@gmail.com>
2021-03-15 20:47:14 -0300

    Desafio do curso de python completo - aula 038

---
+++ VoceGostaDePython.py
@@ -0,0 +1,7 @@
+while True:
+    resposta = input("Você gosta de python? ")
+    if resposta.upper() == 'SIM':
+        print("A resposta está correta")
+        break
+    else:
+        print("Opa, está não é a resposta correta")
